ca7b0209234d0a72a57769dd27c97cb629bc927e
KouichiEndo <129310733+KoichiEndo323@users.noreply.github.com>
2023-04-15 15:18:28 +0000

    [Finish]4-20

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :post_images,only:[:new,:create,:index,:show,:destroy]
-  resources :users,only:[:show,:edit,:update]
+  resources :post_images,only:[:new,:create,:index,:show,:destroy] do
+   resources :post_comments,only:[:create,:destroy]
+   resource :favorites,only:[:create,:destroy]
+  end
 
+   resources :users,only:[:show,:edit,:update]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+
